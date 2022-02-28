fe4b00e6dc0255625b3914760fc5f01b08f87598
Brspontes <brian.robert16@hotmail.com>
2022-02-28 19:09:04 -0300

    ajuste de teste

--- src/tests/unit/freightCalculator.test.ts
+++ src/tests/unit/freightCalculator.test.ts
@@ -2,9 +2,10 @@ import Dimension from "../../domain/entities/dimension";
 import FreightCalculator from "../../domain/entities/freightCalculator";
 import Item from "../../domain/entities/item";
 
-
-test("Deve calcular o frete de um item", function () {
-	const item = new Item(1, "Instrumentos Musicais", "Guitarra", 1000, new Dimension(100, 30, 10), 3)
-	const freight = FreightCalculator.calculate(item, 2)
-	expect(freight).toBe(60)
-});
+describe('Testes de calculo de frete', () => {
+  test("Deve calcular o frete de um item", function () {
+    const item = new Item(1, "Instrumentos Musicais", "Guitarra", 1000, new Dimension(100, 30, 10), 3)
+    const freight = FreightCalculator.calculate(item, 2)
+    expect(freight).toBe(60)
+  });
+})
